7885dea9d579981f4de915779bd4737d8f8d564f
akash22-03 <akashdash2203@gmail.com>
2021-10-07 00:59:01 +0530

    Booking Room Screen

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express();
 const dbconfig = require('./db')
 const roomsRoute = require('./routes/roomsRoute')
 
-app.use('/api/rooms', roomsRoute)
-
+app.use(express.json());
+app.use('/api/rooms', roomsRoute);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
